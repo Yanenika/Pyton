0ae31175591760a892ef32bcfac7831d32161f89
BocharovaYG <nikolenyana@yandex.ru>
2022-12-18 19:58:18 +0300

    task3

--- Lesson2/seminar2.py
+++ Lesson2/seminar2.py
@@ -1,6 +1,15 @@
 # 1. Напишите программу, которая принимает на вход число N 
 # и выдаёт последовательность из N членов.
-def n_func():
-    for i in range(int(input("введите число n "))):
-        print((-3) ** i, end = ' ')
-n_func()
+# def n_func():
+#     for i in range(int(input("введите число n "))):
+#         print((-3) ** i, end = ' ')
+# n_func()
+
+# 2. Создать список, длины n, значения формируются по формуле 3k + 1,
+# где k принимает значения от 1 до n включительно.
+
+# num_list = []
+# n = int(input(" n = "))
+# for k in range (1, n +1):
+#     num_list.append (3 * k + 1)
+# print (num_list)
